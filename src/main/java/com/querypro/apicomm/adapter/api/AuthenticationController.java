649cb6401bc1e73b467e5d25888dcac57df62852
Jorge Del Barco <jdelbarcogarza@gmail.com>
2023-01-19 15:25:46 -0600

    se agrego ruta de localhost

--- src/main/java/com/querypro/apicomm/adapter/api/AuthenticationController.java
+++ src/main/java/com/querypro/apicomm/adapter/api/AuthenticationController.java
@@ -2,14 +2,12 @@ package com.querypro.apicomm.adapter.api;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:5173/", "http://127.0.0.1:5173/"})
 public class AuthenticationController {
 
     private final AuthenticationService service;
